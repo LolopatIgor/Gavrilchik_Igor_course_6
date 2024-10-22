10ceca5d5e5a638fdaa398f4c70aaff27fb260b3
LolopatIgor <Lolopat1994@gmail.com>
2024-10-22 10:24:32 +0200

    Update apps.py

--- notification_app/apps.py
+++ notification_app/apps.py
@@ -2,3 +2,7 @@ from django.apps import AppConfig
 
 class NotificationAppConfig(AppConfig):
     name = 'notification_app'
+
+    def ready(self):
+        from notification_app.jobs import start_scheduler
+        start_scheduler()
